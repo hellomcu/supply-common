b2672bb49c2314e021add8cd754f4c0d937f2815
21415 <liuyang@1hai.cn>
2017-12-22 17:57:44 +0800

    e

--- src/main/java/com/supply/entity/PageInfo.java
+++ src/main/java/com/supply/entity/PageInfo.java
@@ -1,31 +1,36 @@
 package com.supply.entity;
 
+import java.util.List;
+
 import com.supply.entity.base.BaseEntity;
 
-
-public class PageInfo extends BaseEntity
+public class PageInfo<T> extends BaseEntity
 {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 71202666408338674L;
-	//当前页
+	// 当前页
 	private long currentPage = 1;
-	//总页数
+	// 总页数
 	private long totalPage;
-	//每页记录数
+	// 每页记录数
 	private int itemNum = 20;
-	
+	// 总记录数
+	private long totalNum;
+	private List<T> list;
+
 	/**
 	 * 从第几条记录开始(从0开始)
+	 * 
 	 * @return
 	 */
 	public long getStartItemNum()
 	{
 		return currentPage * itemNum - itemNum;
 	}
-	
+
 	public int getItemNum()
 	{
 		return itemNum;
@@ -41,22 +46,53 @@ public class PageInfo extends BaseEntity
 		return currentPage;
 	}
 
-
 	public void setCurrentPage(long currentPage)
 	{
 		this.currentPage = currentPage;
 	}
-
 
 	public long getTotalPage()
 	{
 		return totalPage;
 	}
 
-
 	public void setTotalPage(long totalPage)
 	{
 		this.totalPage = totalPage;
+	}
+
+	public List<T> getList()
+	{
+		return list;
+	}
+
+	public void setList(List<T> list)
+	{
+		this.list = list;
+	}
+
+	public long getTotalNum()
+	{
+		return totalNum;
+	}
+
+	public void setTotalNum(long totalNum)
+	{
+		this.totalNum = totalNum;
+	}
+
+	/**
+	 * 计算共分几页
+	 * 
+	 * @return
+	 */
+	public long calcTotalPage()
+	{
+		if (itemNum == 0)
+		{
+			return 0L;
+		}
+		return Math.round(Math.ceil(totalNum / (double) itemNum));
 	}
 
 }
