8bdb810cecdd7403d20ef0ec37f0f2bf5c7fab30
Your Name <you@example.com>
2017-12-30 13:55:07 +0800

    t

--- src/main/java/com/supply/exception/SupplyException.java
+++ src/main/java/com/supply/exception/SupplyException.java
@@ -7,35 +7,44 @@ public class SupplyException extends RuntimeException
 	 * 
 	 */
 	private static final long serialVersionUID = 5021779341030545803L;
+	
+	private int code;
 
 	public SupplyException()
 	{
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public SupplyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
 	{
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 	public SupplyException(String message, Throwable cause)
 	{
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
+	public SupplyException(String message, int code)
+	{
+		super(message);
+		this.code = code;
+	}
+	
 	public SupplyException(String message)
 	{
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public SupplyException(Throwable cause)
 	{
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
+
+	public int getCode()
+	{
+		return code;
+	}
+
 	
 }
